ce4ffee5f3ebb46d9d44052fb0930bf0920f7b8a
David Rothamel <rothameldavid@gmail.com>
2023-06-29 22:39:25 +0200

    trying to start communication

--- src/common/LandingPage.tsx
+++ src/common/LandingPage.tsx
@@ -4,7 +4,10 @@ import "../common_css/StyleSheet.css"
 //Why wont toggleLayout be accepted as function?
 //Please fix, any is not good
 function LandingPage(props: { toggleLayout: any, decrement: any, increment: any, count: number }) {
-
+    function getData() {
+        fetch("https://localhost:7086/amount_of_players?amount=3").
+            .then((res) => res.status)
+    }
 
     return (
         //Should probably? propably?, probaply? add failsafes for count
@@ -12,7 +15,7 @@ function LandingPage(props: { toggleLayout: any, decrement: any, increment: any,
         <div id="landing">
             <h1>Davids Blackjack</h1>
             <div>
-                <button onClick={props.decrement}>-</button>{props.count}<button onClick={props.increment} >+</button>
+                <button onClick={getData}>-</button>{props.count}<button onClick={props.increment} >+</button>
             </div>
             <button onClick={props.toggleLayout}>Start</button>
       </div>
